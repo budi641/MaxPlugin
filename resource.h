d506ef880993082f91379619d9d3935413b7a2f3
Abddelrahman Ameen <85764244+budi641@users.noreply.github.com>
2025-06-17 00:15:47 +0300

    UI updated

--- resource.h
+++ resource.h
@@ -12,16 +12,27 @@
 #define IDS_CLASS_NONE                      12
 #define IDS_CLASS_INTERACTABLE              13
 #define IDD_CASAVISTAPARAM                  101
+#define IDD_MODEL_SELECT                    102
+#define IDD_MATERIAL_SELECT                 103
 #define IDC_CLASS                           1000
 #define IDC_CLASS_COMBO                     1001
+#define IDC_MODELS_LIST                     1002
+#define IDC_MODELS_ADD                      1003
+#define IDC_MODELS_REMOVE                   1004
+#define IDC_MATERIALS_LIST                  1005
+#define IDC_MATERIALS_ADD                   1006
+#define IDC_MATERIALS_REMOVE                1007
+#define IDC_MATERIAL_INDEX                  1008
+#define IDC_MATERIAL_INDEX_EDIT             1009
+#define IDC_MATERIAL_INDEX_SPIN             1010
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
